5778cff121494699751e4848edae501bf0e0c9fe
KC-Nick <wilson.kasinichole@gmail.com>
2023-10-13 20:16:56 -0400

    adding features, fixing features, and tweaking the css to look better

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# weather-alert
+# Weather Alert
+
+## Description
+
+With this app, I wanted to show what we've learned to apply an API to a web page where one could enter a city and find the weather for the next five days. I also have implemented a feature so that the search history is saved by local storage, and appears on the page in the form of a history list. Clicking on these search history list items will allow the page to show the city's weather again as if they had been searched.
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+
+## Installation
+
+To install this code use `git clone` to clone the repository, `git status` to make sure your copy is up to date or `git pull` to update it.
+
+## Usage
+
+To use this webpage, deploy it here [LINK](https://kc-nick.github.io/weather-alert/), and you can find the Github Repository here [LINK](https://github.com/KC-Nick/weather-alert). This app should appear as the screenshot below, before a search; ![A preview of the website](./assets/loadscreen.png) and when a search is entered, it will appear as such; ![A preview of the website](./assets/searchscreen.png)
+
+## Credits
+
+I would like to give credit and thanks to my wonderful instructor, and tutors. I appreciate all the help in learning and growing in my craft!
--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,6 @@
 var APIKey = '2c5de945f63568dd4dbf3de2fad3fcd4';
-// var queryUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 var temp = document.querySelector('.temp');
-var desc = document.querySelector('.desc');
+var weatherForecast = document.querySelector('.weather-forecast');
 var windSpeed = document.querySelector('.wind-speed');
 var cityName = document.querySelector('.city-name');
 var humidity = document.querySelector('.humidity');
@@ -13,21 +12,54 @@ var todayIcon = document.getElementById('weather-icon')
 $(document).ready(function () {
     function weatherDisplay(data) {
         console.log(data);
-        cityName.textContent = data.city.name;
-        temp.textContent = `Temperature: ${Math.round(data.list[0].main.temp)} degrees.`;
-        humidity.textContent = `Humidity: ${data.list[0].main.humidity} %`;
-        windSpeed.textContent = `Wind Speed: ${Math.round(data.list[0].wind.speed)} mph.`;
-        todayIcon.setAttribute('src', `https://openweathermap.org/img/w/${data.list[0].weather[0].icon}.png`)
+        var html = `<div id="currentDay" class="weather-box">
+        <h2 class="city-name"><strong>${data.city.name} Today;</strong></h2>
+        <p class="temp"><strong>Temp</strong>: ${Math.round(data.list[0].main.temp)} Degrees.</p>
+        <p class="humidity"><strong>Humidity</strong>: ${data.list[0].main.humidity} %</p>
+        <p class="wind-speed"><strong>Wind Speed</strong>: ${Math.round(data.list[0].wind.speed)} MPH.</p>
+        <p class="weather-icon"><strong>Weather</strong>: <img src="https://openweathermap.org/img/w/${data.list[0].weather[0].icon}.png"
+        </div>`
+        weatherForecast.insertAdjacentHTML("afterbegin", html);
+    }
+    // Function to display search history on the page
+    function displaySearchHistory() {
+        // Get the search history from local storage
+        var searchHistory = JSON.parse(localStorage.getItem('searchHistory'));
+        console.log(searchHistory);
+        // Check if there is search history
+        if (searchHistory && searchHistory.length > 0) {
+            // Loop through the search history and display it on the page
+            $('#history').empty();
+            searchHistory.forEach(function (search) {
+                var query = search.query;
+
+                // Display the search history on the page (e.g., append to a list)
+                var historyItem = $('<li>' + query + '</li>').on('click', function () {
+                    // clear out any old html
+                    clearHTML();
+                    getWeather(query);
+                    weatherForecast.style.display = "block";
+                });
+                $("#history").append(historyItem);
+            });
+        }
     }
     function fiveDayDisplay(data) {
         for (let index = 0; index < data.list.length; index = index + 8) {
             console.log(data.list[index]);
-            var html = `<div id="nextDay" class="five-day-forecast weatherBox">
-            <h2 class="temp">${data.list[index].dt_txt.split(" ")[0]}</h2>
-                <p class="desc">Temp: ${Math.round(data.list[index].main.temp)} Degrees.</p>
-                <p class="humidity">Humidity: ${data.list[index].main.humidity} %</p>
-                <p class="wind-speed">Wind Speed: ${Math.round(data.list[index].wind.speed)} MPH.</p>
-                <p class="weather-icon">Weather: <img src="https://openweathermap.org/img/w/${data.list[index].weather[0].icon}.png"
+            var apiDate = data.list[index].dt_txt.split(" ")[0];
+            var dateObj = new Date(apiDate);
+            var formattedDate = dateObj.toLocaleDateString('en-US', {
+                day: 'numeric',
+                month: 'long',
+                year: 'numeric'
+            });
+            var html = `<div class="five-day-forecast weatherBox">
+            <h2 class="today">${formattedDate}</h2>
+                <p class="temp"><strong>Temp</strong>: ${Math.round(data.list[index].main.temp)} Degrees.</p>
+                <p class="humidity"><strong>Humidity</strong>: ${data.list[index].main.humidity} %</p>
+                <p class="wind-speed"><strong>Wind Speed</strong>: ${Math.round(data.list[index].wind.speed)} MPH.</p>
+                <p class="weather-icon"><strong>Weather</strong>: <img src="https://openweathermap.org/img/w/${data.list[index].weather[0].icon}.png"
         </div>`
             fiveDayForecast.insertAdjacentHTML("beforeend", html);
         }
@@ -65,40 +97,21 @@ $(document).ready(function () {
 
         // Store the JSON string in local storage
         localStorage.setItem('searchHistory', searchHistoryString);
+        displaySearchHistory();
     }
 
     // function to clear out the  fiveDayForcast div
     // that runs before we getWeather()
     function clearHTML() {
+        $('.weather-box').empty()
         $('.five-day-forecast').empty()
-    }
-
-    // Function to display search history on the page
-    function displaySearchHistory() {
-        // Get the search history from local storage
-        var searchHistory = JSON.parse(localStorage.getItem('searchHistory'));
-
-        // Check if there is search history
-        if (searchHistory && searchHistory.length > 0) {
-            // Loop through the search history and display it on the page
-            searchHistory.forEach(function (search) {
-                var query = search.query;
-
-                // Display the search history on the page (e.g., append to a list)
-                var historyItem = $('<li>' + query + '</li>').on('click', function () {
-                    // clear out any old html
-                    clearHTML()
-                    getWeather(query);
-                });
-                $("#history").append(historyItem);
-            });
-        }
     }
     $(".submitBtn").on('click', function () {
         citySubmitHistory();
         var city = $('#city-search').val();
-        clearHTML()
+        clearHTML();
         getWeather(city);
+        weatherForecast.style.display = "block";
     });
     displaySearchHistory();
 });
